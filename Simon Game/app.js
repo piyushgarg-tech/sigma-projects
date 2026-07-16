3f61c38a236da57dd2bf4e0767c1e49c8afc8219
Piyush Garg <25f3004745@ds.study.iitm.ac.in>
2026-07-16 22:18:35 +0530

    add touchsreen event also

--- Simon Game/app.js
+++ Simon Game/app.js
@@ -8,13 +8,16 @@ let level = 0;
 
 let h2 = document.querySelector("h2");
 
-document.addEventListener("keypress", function(){
-    if (started == false) {
-        console.log("game started");
+function startGame() {
+    if (!started) {
+        console.log("Game Started");
         started = true;
         levelUp();
-    }  
-})
+    }
+}
+
+document.addEventListener("keypress", startGame);
+document.addEventListener("touchstart", startGame);
 
 function gameFlash(btn){
    btn.classList.add("flash");
